985faab68de2b03384a4015bc3119df5b02f5338
jackjewl <2036891099@qq.com>
2022-06-04 22:45:37 +0800

    others

--- others/hailstone.go
+++ others/hailstone.go
@@ -1,22 +1,7 @@
 package others
 
-func Hailstone(n int) []int {
-	if n <= 1 {
-		return []int{1}
-	}
-
-	if n%2 == 0 {
-		a := []int{n}
-		a = append(a, Hailstone(n/2)...)
-		return a
-	} else {
-		a := []int{n}
-		a = append(a, Hailstone(3*n+1)...)
-		return a
-	}
-}
-
-func HailstoneLength(n int) int {
+//冰雹数序列的长度
+func Hailstone(n int) int {
 	length := 1
 
 	for 1 < n {
